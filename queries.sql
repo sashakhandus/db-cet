8a3fbc3fa669fb48d4cf2e053044096e3a0ccbae
sashakhandus <sashakhandus@gmail.com>
2019-12-13 22:57:42 +0200

    fixed bugs HW 13-14

--- queries.sql
+++ queries.sql
@@ -19,8 +19,17 @@ LEFT JOIN employe AS e ON p.position_id = e.position_id
 INNER JOIN salary AS s ON e.employe_id = s.employe_id
 GROUP BY p.position_id;
 
+
+# 3) get average and highest salary by position; - fixed bug
+SELECT p.position_id,
+       AVG(e.salary_current) AS average_salary_position,
+       MAX(e.salary_current) AS highest_salary_position
+FROM position AS p
+         LEFT JOIN employe AS e ON p.position_id = e.position_id
+GROUP BY p.position_id;
+
 # 4) get total number of days every person worked and total income;
-SELECT SQL_NO_CACHE e.employe_id,
+SELECT e.employe_id,
        COUNT(i.date) AS total_work_days_employe,
        SUM(i.earnings) AS total_income_employe
 FROM employe AS e
@@ -29,14 +38,16 @@ GROUP BY e.employe_id;
 
 
 # 5) get overall (total) income by transport, average income and a number of working days in the descending order;
-SELECT t.transport_id,
-       COUNT(i.date) AS total_work_days_transport,
-       SUM(i.earnings) AS total_income_transport,
-       SUM(i.earnings)/COUNT(i.date) AS average_income_transport
+SELECT SQL_NO_CACHE t.transport_id,
+                    COUNT(i.date) AS total_work_days_transport,
+                    SUM(i.earnings) AS total_income_transport,
+                    AVG(i.earnings) AS average_income_transport
 FROM transport AS t
-LEFT JOIN income AS i ON t.transport_id = i.transport_id
+         LEFT JOIN income AS i ON t.transport_id = i.transport_id
 GROUP BY t.transport_id
 ORDER BY total_income_transport DESC ;
+
+
 
 
 # 6) get people who have birthday in May;
@@ -57,3 +68,7 @@ FROM employe AS e
 WHERE p.position_explanation like 'driver%';
 
 ALTER TABLE income ADD UNIQUE KEY `tran_date` (transport_id, date);
+
+ALTER TABLE employe ADD KEY `passport` (passport);
+
+ALTER TABLE salary ADD KEY `salary_employe` (salary_date, employe_id);
